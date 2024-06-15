9c1b37d98b4a28feb3d139f32dfa3a6e55791bca
Les James <lesjames_nz@hotmail.com>
2024-06-15 23:29:21 +1200

    unoptimized aniated images

--- app/page.tsx
+++ app/page.tsx
@@ -54,36 +54,40 @@ export default function Home() {
 
       <div className=" absolute bottom-0 right-0 z-[10] ">
         <Image 
-          src="/public/horse.png" 
+          src="/horse.png" 
           alt="horse"
           height={300}
           width={300}
           className="absolute right-55 top-40 "
+          unoptimized
         />
 
         <Image
-          src="/public/cliff.webp"
+          src="/cliff.webp"
           alt="cliff"
           width={480}
           height={480}
+          unoptimized
         />
       </div>
 
       <div className=" absolute bottom-0 z-[5] w-full h-auto">
         <Image 
-            src="/public/trees.webp" 
+            src="/trees.webp" 
             alt="trees"
             height={2000}
             width={2000}
             className=" w-full h-full "
+            unoptimized
           />
       </div>
       <Image 
-          src="/public/stars.png" 
+          src="/stars.png" 
           alt="stars"
           height={300}
           width={300}
           className="absolute top-10 left-0 z-[10]"
+          unoptimized
         />
     </main>
   );
