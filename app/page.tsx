522e80da111873d871ab0d627d7b6a3cd9a80fe6
Les James <lesjames_nz@hotmail.com>
2024-06-22 13:26:54 +1200

    addition 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from '@tabler/icons-react'
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,7 +9,15 @@ export default function Home() {
   return (
     <main className=" relative bg-black-100 flex justify-center items-center flex-col overflow-hiddenmx-auto sm:px-10 px-5" >
       <div className=" max-w-7xl w-full ">
-        <Hero></Hero>
+        <FloatingNav
+            navItems={[
+              { name: "Home", link: "/", icon: <IconHome className='h-4 w-4 text-neutral-500 dark:text-white'/> },
+              { name: "About", link: "/", icon: <IconUser className='h-4 w-4 text-neutral-500 dark:text-white'/> },
+              { name: "Contact", link: "/", icon: <IconMessage className='h-4 w-4 text-neutral-500 dark:text-white'/> },
+            ]}
+          />
+        <Hero/>
+        <Grid/>
       </div>
     </main>
   );
